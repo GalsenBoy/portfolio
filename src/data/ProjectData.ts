623ccd94962239c7054b8862847d16abb1e8df61
GalsenBoy <bilaly.cissokho21@gmail.com>
2025-11-15 19:50:02 +0100

    suppression des images inutilisées

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -6,22 +6,10 @@ export const projects = [
     alt: "taqwa planner"
   },
   {
-    name: "/fitsuivi.webp",
-    valeur: "FITSUIVI",
-    to: "https://fit-suivi.vercel.app/",
-    alt: "Suivi sportif"
+    name: "/orygo.webp",
+    valeur: "Orygo",
+    to: "https://orygo.app/",
+    alt: "orygo app"
   },
-  {
-    name: "/bet.webp",
-    valeur: "ELITE BET HUB",
-    to: "https://elite-bet-hub.vercel.app/",
-    alt: "elitebethub"
-  },
-
-  {
-    name: "/github.webp",
-    valeur: "VOIR D'AUTRES PROJECTS",
-    to: "https://github.com/GalsenBoy?tab=repositories",
-    alt: "github"
-  },
+ 
 ];
