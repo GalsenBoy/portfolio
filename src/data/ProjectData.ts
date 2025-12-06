094ee54a0d23ce1687ed2cc08c2626b8ce67fb88
GalsenBoy <bilaly.cissokho21@gmail.com>
2025-12-06 14:21:12 +0100

    optimisation taille des projets

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,15 +1,14 @@
 export const projects = [
   {
-    name: "/taqwa.webp",
-    valeur: "Taqwa Planner",
-    to: "https://taqwa-planner.vercel.app/",
-    alt: "Capture d'écran de l'application Taqwa Planner - Planificateur pour musulmans"
-  },
-  {
     name: "/orygo.webp",
     valeur: "Orygo",
     to: "https://orygo.app/",
     alt: "Capture d'écran de l'application Orygo - Application web moderne"
   },
-
+  {
+    name: "/taqwa.webp",
+    valeur: "Taqwa Planner",
+    to: "https://taqwa-planner.vercel.app/",
+    alt: "Capture d'écran de l'application Taqwa Planner - Planificateur pour musulmans"
+  }
 ];
