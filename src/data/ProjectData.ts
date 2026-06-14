5f73f3ad9d2ac465241cf5f94558797862af1903
Galsenboy <bilaly.cissokho21@gmail.com>
2026-06-14 11:37:37 +0200

    ajout de projet tap 1-100

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -16,5 +16,11 @@ export const projects = [
     valeur: "Taqwa Planner",
     to: "https://taqwa-planner.vercel.app/",
     alt: "Taqwa Planner - Application de planification pour les musulmans"
-  }
+  },
+  {
+    name: "/tap.webp",
+    valeur: "Tap 1-100",
+    to: "https://tap-1-100.vercel.app/",
+    alt: "Tap 1-100 - Tapez les chiffres de 1 à 100 le plus rapidement possible - Jeu de rapidité"
+  },
 ];
