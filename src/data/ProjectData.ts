e85385eccf8d5c0977ec0441fd3720a203f21941
Galsenboy <bilaly.cissokho21@gmail.com>
2026-06-14 11:39:27 +0200

    ajout de projet tap 1-100

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -20,7 +20,7 @@ export const projects = [
   {
     name: "/tap.webp",
     valeur: "Tap 1-100",
-    to: "https://tap-1-100.vercel.app/",
+    to: "https://tap-1-100.com/",
     alt: "Tap 1-100 - Tapez les chiffres de 1 à 100 le plus rapidement possible - Jeu de rapidité"
   },
 ];
